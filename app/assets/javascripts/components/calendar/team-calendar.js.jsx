6b7852bda6a1f86f79070297a75fed81d2822c4b
Billy Littlefield <billylittlefield@gmail.com>
2015-10-18 16:49:05 -0700

    change site header to JogLog

--- app/assets/javascripts/components/calendar/team-calendar.js.jsx
+++ app/assets/javascripts/components/calendar/team-calendar.js.jsx
@@ -55,8 +55,8 @@ window.TeamCalendar = React.createClass({
             <th colSpan="3">
               <span className="left-arrow" onClick={this.previousWeek}>&#9654;</span>
               <span>{weekStart.format("MMM DD") + " - " +
-               weekStart.clone().add(7, "days").format("MMM DD")}</span>
-              <span className="right-arrow" onClick={this.previousWeek}>&#9654;</span>
+               weekStart.clone().add(6, "days").format("MMM DD")}</span>
+              <span className="right-arrow" onClick={this.nextWeek}>&#9654;</span>
             </th>
             <th className="join-button" colSpan="3">
               <JoinTeamButton weekStart={this.state.weekStart.clone()}
